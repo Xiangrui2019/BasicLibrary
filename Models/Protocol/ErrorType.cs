9620574d35cb08ce86433e3403060796001849bf
HomeServing <xiangrui@aiursoft.com>
2020-10-06 11:55:58 +0800

    Add sln files

---
+++ Models/Protocol/ErrorType.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicLibrary.Models.Protocol
+{
+    /**
+     * 错误码:
+     * - "0" 为成功
+     * - "+x" 为失败, 且原因是用户输入错误
+     * - "-x" 为失败, 且原因是服务器本身错误
+     */
+    public enum ErrorType
+    {
+        Success = 0,
+        
+    }
+}
---
+++ Models/Protocol/MessageProtocol.cs
@@ -0,0 +1,8 @@
+﻿namespace BasicLibrary.Models.Protocol
+{
+    public class MessageProtocol
+    {
+        public virtual ErrorType Code { get; set; }
+        public virtual string Message { get; set; }
+    }
+}
